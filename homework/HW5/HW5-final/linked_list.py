c6edea3089a6e2d620060c58d71c8167bc809eb1
Yingchen Liu <yingchenliu@Yingchens-MacBook-Pro.local>
2020-11-10 11:20:24 -0700

    Add files for Problem2.

---
+++ homework/HW5/HW5-final/linked_list.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 15:29:49 2020
+
+@author: yingchenliu
+"""
+
+class LinkedList:
+
+    def __init__(self, head, tail):
+        assert isinstance(tail, LinkedList) or isinstance(tail, Nil), TypeError(
+            'tail should either be a LinkedList or a Nil')
+        self._head, self._tail = head, tail
+
+    @property
+    def head(self):
+        return self._head
+
+    @property
+    def tail(self):
+        return self._tail
+
+    def __str__(self):
+        return str(self._head) + ' -> ' + str(self._tail)
+
+    def __repr__(self):
+        return f'LinkedList({repr(self._head)}, {repr(self._tail)})'
+
+    def __len__(self):
+        return 1 + len(self._tail)
+
+    def __getitem__(self, i):
+        return self._head if i == 0 else self._tail[i-1]
+
+    def prepend(self, val):
+        return LinkedList(val,self._tail.prepend(self._head) )
+
+    def append(self, val):
+        return LinkedList(self._head, self._tail.append(val))
+  
+    
+    def for_each(self, fun):
+        return LinkedList(fun(self._head), self._tail.for_each(fun))
+    
+    
+    def summation(self):
+        return self._head + self._tail.summation() if self._tail else self._head
+        return self._head + self._tail.summation() if self._tail else self._head
+    
+    def minimum(self):
+        def smaller(a, b):
+            return a if a < b else b
+        return smaller(self._head, self._tail.minimum()) if self._tail else self._head
+
+    def reduce_right(self, fun):
+        return fun(self._head, self._tail.reduce_right(fun)) if self._tail else self._head
+
+
+
+class Nil():
+
+    def __str__(self): 
+        return 'Nil'
+
+    def __repr__(self):
+        return 'Nil()'
+
+    def __len__(self):
+        return 0
+
+    def __getitem__(self, i):
+        raise IndexError('index out of range')
+
+    def __bool__(self): 
+        return False
+
+    def prepend(self, val): 
+        return LinkedList(val, Nil())
+
+    def append(self, val):  
+        return LinkedList(val, Nil())
+
+    def for_each(self, fun):
+        return Nil()
+# #demo
+# if __name__ == '__main__':
+#     #demo part a
+#     print('-----part A-----')
+#     l_1 = Nil().append(1).append(2).append(3).append(4) 
+#     l_2 = Nil().prepend(1).prepend(2).prepend(3).prepend(4)
+
+#     print('append:', l_1)
+#     print('prepend: ', l_2)
+  
+#     #demo part b
+#     print('-----part B-----')
+#     def square(x):
+#         return x**2
+#     print('append(squared):', l_1.for_each(square))
+#     print('prepend(squared): ',l_2.for_each(square))
+    
+#     #demo part c
+#     print('-----part C-----')
+#     l = Nil().prepend(1).prepend(2).prepend(3).prepend(4)
+#     def smaller(a, b): # our "combine" function
+#         return a if a < b else b
+#     print(l)
+#     print('reduce_right(smaller):',l.reduce_right(smaller))
+    
+    
+    
+    
+    
+    
